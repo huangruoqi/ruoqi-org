f2241bf7ddfe7299241ded634ce50f803cdb4836
Ruoqi Huang <huangruoqi374@gmail.com>
2023-04-23 16:26:09 -0700

    add torch and return sentiment

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 import json
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from transformers import pipeline
+
+sentiment_pipeline = pipeline("sentiment-analysis")
 
 @api_view(['POST'])
 def submit_text(request):
@@ -10,13 +13,14 @@ def submit_text(request):
         text = json.loads(request.body).get('text', None)
 
         if text:
-            response = {'success': True, 'message': 'Text received: {}'.format(text)}
+            response = {'success': True, 'message': 'Text received: {}'.format(text), 'sentiment':sentiment_pipeline([text])}
         else:
             response = {'success': False, 'message': 'No text received'}
 
         return JsonResponse(response)
     else:
         return JsonResponse({'success': False, 'message': 'Invalid request method'})
+
 
 urlpatterns = [
     path("submit-text", submit_text),
